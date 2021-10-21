e364387503bdd77f038f29ff5c9b1a3f42e035c1
unknown <garcovicente@gmail.com>
2021-10-20 21:40:38 -0500

    update

---
+++ Programacion Basica/main.js
@@ -0,0 +1,43 @@
+/* console.log('Hello World'); */
+// Este es un comentario
+/* este es un comentario
+en varias lineas de 
+codigo */
+
+/* var cualquiera = 131234134123413241234134;
+var puntoDecimal = 098098098.123;
+var numeroNegativo = -123123123;
+var string = 'Hola Mundo';
+
+
+
+console.log(cualquiera);
+console.log(puntoDecimal);
+console.log(numeroNegativo);
+console.log(string);
+ */
+
+/* var loQueSea = 30;
+console.log(loQueSea);
+
+
+loQueSea =  23;
+console.log(loQueSea); */
+/* 
+var equis = 20;
+var equis2 = equis;
+
+console.log(equis2); */
+
+var num1 = 15;
+var num2 = 2;
+var suma = num2 % num1;
+
+/* console.log(suma); */
+
+/* operadores de texto */
+
+var nombre = 'pedro';
+var lastName = 'perez';
+
+console.log(name)
---
+++ Programacion Basica/main2.js
@@ -0,0 +1,46 @@
+/* var equis1 = 15;
+var equis2 = 15;
+var nombre = '';
+ */
+/* operadores de comparacion */
+
+/* 
+if ( equis1 < equis2){
+    console.log('Bienvenido')
+
+} */
+
+
+/* var nombre = prompt('Cual es tu nombre?')
+ */
+/* crear un algoritmo que le pida al usuario su nombre y su edad 
+y si es mayor de edad te dara un mensaje de bienvenida con un alert
+
+*/
+
+/* alert('Alto ahi todavia no puedes ver nopor')
+var nickname = prompt('cual es tu nombre?');
+
+)
+
+if ( age > 18  ) {
+    alert = ('Bienvenido señor');
+} else {
+
+    greeting = "Adios!"
+}
+ */
+
+
+var surname = '';
+var age = '';
+var key = 18;
+
+surname = prompt('Cual es tu nombre?')
+age = prompt('cual es tu edad?')
+
+if (age > key) {
+    alert('Acceso consedido, disfrute su nopor')
+} else {
+    alert('Largo de aqui mocoso')
+}
