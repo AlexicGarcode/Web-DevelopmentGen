7d8f50f6c36f05343ae7e392f105f6359b8fa608
unknown <garcovicente@gmail.com>
2021-10-27 21:30:24 -0500

    bucles

---
+++ Programacion Basica/Clase 2/main.js
@@ -0,0 +1,138 @@
+/* var gato = true;
+var pajaro = false; 
+
+/* if(pajaro){
+    console.log('Tiene 4 patas');
+}
+else{
+    console.log('No tiene 4 patas')
+} */
+/* 
+var data_entry = '';
+var validation = 18;
+
+alert('Hello Stranger');
+
+data_entry = prompt('Enter a 2 digits number');
+
+if (data_entry == validation) {
+    alert('Welcome Mister Molotov');
+}
+else{
+    alert('El FBI viene por ti');
+} */
+
+/* var dinero = prompt('Cuanto dinero traes?') */
+
+
+/* var estudiante = 'Sebas';
+var titulo = true;
+var experiencia = false;
+var edad = 20;
+
+if ((titulo || experiencia ) && edad > 18 ) {
+
+    console.log('Puede conseguir trabajo');
+
+}
+
+else{
+    console.log('No Pude conseguir trabajo');
+}
+ */
+
+
+/*
+
+si el usuario tien cupon se le descuenta 1 dolar .1$
+
+
+El helado sin topping cuesta 1.90€.
+El topping de oreo cuesta 1€.
+El topping de KitKat cuesta 1.50€.
+El topping de brownie cuesta 0.75€.
+El topping de lacasitos cuesta 0.95€.
+
+
+
+
+/* */
+
+/* alert('Bienvenido a heladeria Fernandez');
+
+var qty = prompt('Cuantos helados va a llevar hoy?');
+var topping_validation = prompt('Quieres algun topping?');
+var topping_yes = prompt('Que topping te ponemos?')
+var coupon = prompt('Tienes algun coupon?');
+/* var base_price = 1.90
+var oreo = 1.00
+var KitKat = 1.50
+var brownie = 0.75
+var lacasitos = 0.95 */
+/*var icecream = 1.90;
+var coupon = -1.00;
+var total = 0.00;
+total = (qty * icecream) + price - coupon;
+
+if(topping_validation == 'No' || 'no' || 'nO' || 'No'){
+    alert('Por favor elige una cantidad de helados');
+    }
+    else if (topping_yes == 'oreo') {
+        price = 1;
+    }else if(topping_yes == 'KitKat'){
+        price = 1.50;
+    }else if(topping_yes == 'brownie'){
+        price = .75;
+    }else if(topping_yes == 'lacasitos'){
+        price = .95;
+}else{
+    alert('Elige un toping');
+}
+
+alert('Su helado cuesta ' + total + ' $'); */
+
+
+//default se usa para cuando no escriben bien lo que se
+
+
+
+/* var helado = 1.90;
+var qty = '';
+var coupon = -1.00;
+var total = 0.00;
+total = */
+
+
+/* var dinero = prompt('cuanto dinero traes?'); */
+
+
+/* if (dinero > 100){
+    console.log('te alcanza para 10 panes de morido');
+}else if (dinero >= 90){
+    console.log('Te alcanza para 9 panes de morido');
+}else if (dinero >= 80){
+    console.log('Te alcanza para 8 panes de morido');
+}else if (dinero >= 70){
+    console.log('Te alcanza para 7 panes de morido');
+}else{
+    console.log('No moleste');
+} */
+
+
+/* 
+==
+===
+>
+<
+>=
+<=
+!= differente de
+*/
+
+
+var perro = prompt('Cuanto dinero tienes?');
+
+if (perro != 0) {
+    console.log('Si tienes dinero');
+
+}
---
+++ Programacion Basica/Clase 3/main.js
@@ -0,0 +1,114 @@
+/* var loQueSea = 30;
+
+switch (loQueSea) {
+    case 30: 
+        console.log('hee hee');
+    break;
+    case 40: 
+        console.log('40');
+
+    default:
+        console.log('default');
+        break;
+}
+ */
+/* if (loQueSea == 30) {
+    console.log('hee hee');
+}else if(loQueSea == 40){
+    console.log('40');
+}else{
+
+} */
+
+
+//BUCLES
+
+/* es cuando tenemos una linea de 
+codigo y la queremos repetir varias veces
+para eso se usa el for 
+
+Cuando estas repitiendo el mismo codigo ya estas
+haciendo algo mas
+
+Siempre que se vaya a repetir algo se va a usar un 
+bucle
+
+*/
+// Template strings
+//cuando tienes una imagen dentro de un string se llama template string
+
+/* var img = '<img src="img/skull.png" alt="calaca">'
+
+for(let index = 0; index < 500; index ++){
+    document.write(img)
+
+} */
+
+/* document.write('1');
+document.write('2');
+document.write('3');
+document.write('4');
+document.write('5'); */
+
+/* 1 x = 10
+2 x = 20
+3 x = 30
+4 x = 40
+5 x = 50
+6 x = 60
+7 x = 70
+8 x = 80
+9 x = 90
+10 x = 100 */
+
+/* var multiply = 'x'; */
+
+var tabla = 1;
+
+/* for (let index = 1; index <= 10; index++) { */
+/* 
+    document.write('<h2>' + index + ' x' + ' 10' + ' = ' + (index * 10) + '</h2>'); */
+    
+    /* document.write('<h2>' + index + ' x ' + tabla + ' = ' + (index * tabla) + '</h2>'); */
+
+    /* document.write('<h2>' + tabla + ' x ' + index + ' = ' + (index * tabla) + '</h2>');
+} */
+
+/* for (let index2 = 1; index2 < 10; index2++) {
+
+        console.log(index2);
+    
+    for (let index = 1; index <= 10; index++) {
+        document.write('<h2>' + tabla + ' x ' + index + ' = ' + (index * tabla) + '</h2>');
+    }
+} */
+
+
+/* for (let index2 = 1; index2 <= 10; index2++) { */
+
+   /*  console.log(index2); */
+
+/*     for (let index = 1; index <= 10; index++) {
+        /* console.log(index); */
+      //  document.write('<h2>' + index2 + ' x ' + index + ' = ' + (index * index2) + '</h2>');
+//   }
+//} */
+
+
+
+
+var data = prompt('dame un numero')
+var happy = '$';
+
+
+for (let index1 = 0; index1 < data ; index1++) {
+    document.write('<h2>' + happy + '</h2>');
+
+
+
+    for (let index1 = 0; index1 < data ; index1++) {
+        document.write('<h2>' + happy  + happy + '</h2>');
+    }
+   
+
+}
