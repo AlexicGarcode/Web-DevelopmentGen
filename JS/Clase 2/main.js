5a37828e3c0d7c0bdc9e559230c6b6895bdf5dd3
unknown <garcovicente@gmail.com>
2021-11-10 21:40:38 -0600

    ejercicio de inyectar cosas

---
+++ JS/Clase 2/main.js
@@ -0,0 +1,50 @@
+
+
+/* var element = '<div>Algo de texto </div>'
+
+document.getElementsByTagName('body')[0].innerHTML = element; */
+
+/* 
+var container = document.getElementsByTagName('main')[0];
+var animals = [
+    'Perro',
+    'Kiwi',
+    'Dodo',
+    'Aguila',
+    'Condor',
+    'Lobo',
+    'Tiburon',
+]; */
+
+/* var element = document.createElement('div');
+
+container.appendChild(element); */
+
+/* 
+for (let index = 0; index < animals.length; index++) {
+    var element = document.createElement('div');
+    element.innerHTML = `
+
+    <div>
+    
+    <p> ${animals[index]} </p>
+
+    <div>
+    
+    `;
+    container.appendChild(element);
+    
+}
+
+    
+ */
+
+var tiktoker = {
+    name: 'luis angel',
+    stars: 10,
+    type: 'Frio/ADA',
+    info: 'Este mosntruo de angular, se alimenta de JavaScript, su debilidad son los corridos tumbados',
+    atk: 1200,
+    def: 900,
+    img: 'IMG-20211005-WA0001.jpg',
+};
