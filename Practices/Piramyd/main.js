458fa60ede086aa5714a745c6beedca944bf68ed
unknown <garcovicente@gmail.com>
2021-11-17 19:18:24 -0600

    ejercicio tik tokers

---
+++ Practices/Piramyd/main.js
@@ -0,0 +1,12 @@
+var num = prompt('Dame un numero');
+
+
+for (let index2 = 1; index2 < num; index2++) {
+    
+    for (let index = 1; index < index2; index++) {
+        
+        document.write('$');
+        
+    }
+    document.write('<br>');
+}
