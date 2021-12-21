dfe9ffea1f22332d5b22edbfcd84c3c768b50599
unknown <garcovicente@gmail.com>
2021-12-20 20:57:09 -0600

    POO

---
+++ JS/Clase 10/main.js
@@ -0,0 +1,51 @@
+const NoCliente = ' ';
+/* const NoCliente = 'a9s8d9a71232132133';
+ */
+/* 
+let num1 = 15;
+
+
+let num2 = 20;
+console.log(num2);
+
+
+{let num2 = 50;
+console.log(num2); 
+}
+
+
+
+if(num2 == 20) {
+    console.log('hola mundo');
+
+}else{
+    console.log('puto el que lo lea')
+} */
+
+/* mensaje();
+
+function mensaje(){
+    document.write('hee hee');
+}
+
+
+const perro = function(){
+    document.write('perro');
+}
+
+perro();
+*/
+
+const perro = function(nombre){
+    document.write(nombre);
+}
+
+const gato = function(){
+
+    document.write('gato');
+
+
+}
+
+
+gato();
---
+++ JS/Clase 12/main.js
@@ -0,0 +1,31 @@
+let perro = {
+    name: 'Pugberto',
+    edad: 7,
+    color: 'azul',
+};
+
+
+
+
+/* 
+const gatos = (name, age, color) => {
+
+    let element ={
+        name: name,
+        age: age,
+        color: color,
+    }
+
+    return element;
+
+}
+
+let gato1 = gatos('gato', 9,'cafe'); */
+
+function gatos(name, age, color){
+    this.name = name;
+    this.age = age;
+    this.color = color;
+}
+
+let gato1 =new gatos('gato',9,'cafe');
