7a0c0d0eca8ae75167fe597dfcfa5b59e7b5cdff
unknown <garcovicente@gmail.com>
2022-01-03 21:18:40 -0600

    JSON

---
+++ JS/Clase14/main.js
@@ -0,0 +1,18 @@
+/* let window = window.innerWidth;
+let window = 
+
+
+// para sacar altura y ancho de la ventana
+screen.width; 
+screen.height;
+ */
+
+const frutas = "['naranja','platano', 'manzana']";
+
+const frutas2 = '["naranja", "platano", "manzana"]';
+
+const frutasArray = JSON.stringify(frutas);
+
+console.log(frutas);
+console.log(frutas2);
+console.log(frutasArray);
