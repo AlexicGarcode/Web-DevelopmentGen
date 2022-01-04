2ffb9453a600f998bf6e8754ec4cce645660b1ac
unknown <garcovicente@gmail.com>
2022-01-03 19:41:36 -0600

    babel

--- JS/Clase 12/main.js
+++ JS/Clase 12/main.js
@@ -26,6 +26,9 @@ function gatos(name, age, color){
     this.name = name;
     this.age = age;
     this.color = color;
+    this.netName = function(){
+        
+    }
 }
 
 let gato1 =new gatos('gato',9,'cafe');
---
+++ JS/Clase 13/js/ecma/main.js
@@ -0,0 +1,5 @@
+const perro = () => {
+    return 10;
+}
+
+let duck = '2';
